e338b5e9c0c815a15ce00bb9f33f6c7aec0cffb2
ratel <rubtub2314@gmail.com>
2025-08-29 12:08:36 +0900

    오타 제거

--- Assets/Project/GameManager.cs
+++ Assets/Project/GameManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : SingleTon<GameManager>
 {
-    public GameSceneControl GaeSceneControl { get; private set; }
+    public GameSceneControl GameSceneControl { get; private set; }
 
     private void Start()
     {
@@ -21,7 +21,7 @@ public class GameManager : SingleTon<GameManager>
 
     private void OnSceneLoaded()
     {
-        GaeSceneControl = GameObject.FindGameObjectWithTag("SceneControl").GetComponent<GameSceneControl>(); // 씬 컨트롤 캐싱
-        GaeSceneControl.Initialize(Time.time + 60f);
+        GameSceneControl = GameObject.FindGameObjectWithTag("SceneControl").GetComponent<GameSceneControl>(); // 씬 컨트롤 캐싱
+        GameSceneControl.Initialize(Time.time + 60f);
     }
 }
